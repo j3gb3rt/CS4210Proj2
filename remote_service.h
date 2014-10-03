ceda59a55dc30a7e87d1e15712697afb6f8a3967
chrisenuf <chrisenuf@gmail.com>
2014-10-02 23:37:49 -0400

    message queue stuff

--- remote_service.h
+++ remote_service.h
@@ -1,21 +1,26 @@
 #include <pthread.h>
 #include <sys/types.h>
 
+#define MSG_SIZE 128
+
 typedef struct __arg_stack
 {
 	int arg0;
-    int arg1;
+	int arg1;
 } arg_stack;
-
-
 
 typedef struct __shared_block
 {
 	int locked;
-    int arg0;
+	int arg0;
 	int arg1;
    	int ret_val;
 } shared_block;
+
+typedef struct __msg {
+	long mtype;
+	char mtext[MSG_SIZE];
+} msg;
 
 int remote_service_server_init();
 int remote_service_client_init();
