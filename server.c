ac31ed44ca1fd9b0bb0c7afa7119f1b0f747ba36
chrisenuf <chrisenuf@gmail.com>
2014-10-05 21:04:58 -0400

    implemented adding to process queue

--- server.c
+++ server.c
@@ -2,7 +2,7 @@
 #include <pthread.h>
 
 pthread_t msg_watcher;
-
+process_queue queue;
 
 
 void msgq_watch()
@@ -13,10 +13,64 @@ void msgq_watch()
 	{
 		msgq_rcvr(message);
 		memcpy(key, message.mtext,[MSG_SIZE]); 
-		/*TODO add request to request queue*/
-			
+		add_to_queue(key, (pid_t)(message.mtype));		
+	}
 }
 
+void add_to_queue(key_t key, pid_t pid)
+{
+	process_queue curr = queue;
+	if(curr == NULL)
+	{
+		//if process queue is NULL, create first node
+		queue = (process_queue *)malloc(sizeof(process_queue));
+		queue.pid = pid;
+		queue->last = queue;
+		curr = queue;
+	}	
+
+	//iterate through the process queue, until pid is a match
+	while(curr->next != NULL)
+	{
+		if(curr.pid != pid)
+		{
+			curr = curr->next;
+		}else
+		{
+			break;
+		}
+	}
+
+	//process does not exist in queue, so add it
+	if(curr.pid != pid)
+	{
+		queue->last->next = (process_queue *)malloc(sizeof(process_queue));
+		queue->last = queue->last->next;
+		queue->last.pid = pid;
+		curr = queue->last;
+	}
+
+	add_to_requestq(curr, key)
+}
+
+void add_to_requestq(process_queue queue, key_t key)
+{
+	request_queue curr = queue->requests;
+	//request queue empty, add to front
+	if(curr == NULL)
+	{
+		queue->requests = (request_queue *)malloc(sizeof(request_queue));
+		queue->requests->last = queue->requests;
+		queue->requests.shared_mem_key = key;	
+	}else
+	{
+	//create and add to end of request queue
+		curr = (request_queue *)malloc(sizeof(request_queue));
+		curr.shared_mem_key = key;
+		queue->requests->last->next = curr;
+		queue->request->last = curr;
+	}
+}
 
 
 main(1)
@@ -24,5 +78,5 @@ main(1)
 	remote_service_server_init();
 	pthread_create(&msg_watcher, NULL, &msg_watch, NULL);
         fprint("message queue watching thread created\n"); 
-	
+	/*TODO service requests*/
 }
